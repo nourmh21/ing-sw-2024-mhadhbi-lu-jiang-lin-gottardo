e884dbe8b5b5de0335e62fb10f21ceb02a8edf4d
Valeria Lu <valeria.lu@mail.polimi.it>
2024-04-16 00:32:14 +0200

    modify Desk class, add new test for Desk class

--- CodexNaturalis/src/test/java/it/polimi/ingsw/DeskTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/DeskTest.java
@@ -2,9 +2,12 @@ package it.polimi.ingsw;
 
 import it.polimi.ingsw.model.Desk;
 import it.polimi.ingsw.model.enums.CardType;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class DeskTest {
 
@@ -52,6 +55,85 @@ public class DeskTest {
     }
 
 
+    @Test
+    public void pickDisplayedNextRCard_PickCard_ReallyOut(){
+        List<Integer> testList = desk.getDisplayedRCards();
+        int initialSize = testList.size();
+        desk.pickOneDisplayedRCard(testList.getFirst());
+        assertEquals(testList.size(), initialSize - 1);
+
+    }
+
+    @Test
+    public void pickDisplayedNextGCard_PickCard_ReallyOut(){
+        List<Integer> testList = desk.getDisplayedGCards();
+        int initialSize = testList.size();
+        desk.pickOneDisplayedGCard(testList.getFirst());
+        assertEquals(testList.size(), initialSize - 1);
+
+    }
+
+
+    @Test
+    public void pickDisplayedNextRCard_PickCardWithUpdate_ReallyUpdated(){
+        List<Integer> testList = desk.getDisplayedRCards();
+        desk.pickOneDisplayedRCard(testList.getFirst());
+        int sizeBeforeUpdating = testList.size();
+        desk.updateDisplayedRCard();
+        assertEquals(testList.size(), sizeBeforeUpdating + 1);
+    }
+
+
+    @Test
+    public void pickDisplayedNextGCard_PickCardWithUpdate_ReallyUpdated(){
+        List<Integer> testList = desk.getDisplayedGCards();
+        desk.pickOneDisplayedGCard(testList.getFirst());
+        int sizeBeforeUpdating = testList.size();
+        desk.updateDisplayedGCard();
+        assertEquals(testList.size(), sizeBeforeUpdating + 1);
+    }
+
+
+    @Test
+    public void updateNextRCard_PickTwoTimes_ShouldNotBeTheSame(){
+        int card1 = desk.pickNextRCard();
+        desk.updateNextRCard();
+        int card2 = desk.pickNextRCard();
+        assertNotEquals(card1, card2);
+    }
+
+
+    @Test
+    public void updateNextGCard_PickTwoTimes_ShouldNotBeTheSame(){
+        int card1 = desk.pickNextGCard();
+        desk.updateNextGCard();
+        int card2 = desk.pickNextGCard();
+        assertNotEquals(card1, card2);
+    }
+
+
+    @Test
+    public void pickNextRCard_PickCardWithoutUpdate_ShouldGiveUsNull(){
+        desk.pickNextRCard();
+        assertNull(desk.pickNextRCard());
+    }
+
+
+    @Test
+    public void pickNextGCard_PickCardWithoutUpdate_ShouldGiveUsNull(){
+        desk.pickNextGCard();
+        assertNull(desk.pickNextGCard());
+    }
+
+
+    @Test(expected = NullPointerException.class)
+    public void pickOneCard_CalledByUpdatingMethodWhenTheDeckIsEmpty_throwsNullPointerException(){
+        for (int i = 0; i < 37; i++){
+            desk.pickOneCard(CardType.RESOURCE);
+        }
+        desk.pickOneDisplayedRCard(desk.getDisplayedRCards().getFirst());
+        desk.updateDisplayedRCard();
+    }
 
 }
 
