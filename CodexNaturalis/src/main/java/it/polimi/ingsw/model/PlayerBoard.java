548b9615970afa364a43eaa3dec2d2080274e370
nourmh21 <nourhane.mhadhbi@mail.polimi.it>
2024-04-23 10:13:05 +0200

    update PlayerBoard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerBoard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerBoard.java
@@ -15,7 +15,7 @@ public class PlayerBoard {
     final private List<Symbol> bottomLeftAngle;
     final private List<Symbol> bottomRightAngle;
     final private List<Symbol> cardKingdom;
-    final private List<Integer> x;
+    final private List<Integer>  x;
     final private List<Integer> y;
     final private int[] symbolsList;
     final private ArrayList<int[]> availablePosition;
