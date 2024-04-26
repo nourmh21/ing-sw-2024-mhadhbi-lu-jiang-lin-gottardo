669be5b7d7645cfb57aedafeb8053870b21d6654
Jiang <elena.jiang@mail.polimi.it>
2024-04-26 14:39:06 +0200

    change runtimeException in Exception

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/exceptions/InvalidNumOfConnectedPlayer.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/exceptions/InvalidNumOfConnectedPlayer.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.model.exceptions;
 
 /**InvalidNumOfPlayer defines an exception when there is a number that fails to start the game */
 
-public class InvalidNumOfConnectedPlayer extends RuntimeException {
+public class InvalidNumOfConnectedPlayer extends Exception{
     public InvalidNumOfConnectedPlayer(){
         super();
     }
