94a01b2eee6cf42809418bb11f95d9f0bf5410b9
Valeria Lu <valeria.lu@mail.polimi.it>
2024-05-09 22:24:36 +0200

    add some class for socket use and server main

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/Server.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.network;
+
+
+import it.polimi.ingsw.model.CardList;
+import it.polimi.ingsw.network.socket.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static CardList cardList;
+    private static final int port = 49251;
+    public static void main(String[] args){
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                try {
+
+                    Socket socket = serverSocket.accept();
+                    new ClientHandler(socket).start();
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+
+
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/SendHeartbeat.java
@@ -0,0 +1,46 @@
+package it.polimi.ingsw.network.socket;
+
+import it.polimi.ingsw.message.enums.MessageType;
+import it.polimi.ingsw.message.HeartbeatMessage;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.SocketException;
+
+
+public class SendHeartbeat extends Thread{
+
+    private ObjectOutputStream oos;
+    private long lastSendTime;
+
+    public SendHeartbeat(ObjectOutputStream oos){
+        this.oos = oos;
+        lastSendTime = System.currentTimeMillis();
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (true){
+                //there are decisions to be made here
+                Thread.sleep(1*1000);
+                if ((System.currentTimeMillis() - lastSendTime) >= 3*1000){
+                    oos.writeObject(new HeartbeatMessage(MessageType.HEARTBEAT));
+                    lastSendTime = System.currentTimeMillis();
+                }
+
+            }
+
+        } catch (SocketException e){
+            System.out.println("You lose connection to server");
+        }catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
