affb8fe15140fd1cb5e1213f9e9cf5675135e0b8
Valeria Lu <valeria.lu@mail.polimi.it>
2024-05-17 23:10:09 +0200

    update immutable classes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/ImmutableLobby.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/ImmutableLobby.java
@@ -1,8 +1,9 @@
 package it.polimi.ingsw.controller;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class ImmutableLobby {
+public class ImmutableLobby implements Serializable {
     private final List<String> players;
     private final int numOfPlayer;
     public ImmutableLobby(Lobby lobby){
