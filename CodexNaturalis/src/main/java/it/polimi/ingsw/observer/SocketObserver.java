aba2e63d71ec7bc47b2ba1dd5c5cc643e2235b36
Valeria Lu <valeria.lu@mail.polimi.it>
2024-05-18 02:41:38 +0200

    add new observer class, update already existing observer classes

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/observer/SocketObserver.java
@@ -0,0 +1,106 @@
+package it.polimi.ingsw.observer;
+
+import it.polimi.ingsw.controller.server.ImmutableLobby;
+import it.polimi.ingsw.message.enums.NotifyType;
+import it.polimi.ingsw.message.notify.NotifyMessage;
+import it.polimi.ingsw.model.immutable.ImmutableEndGameInfo;
+import it.polimi.ingsw.model.immutable.ImmutableGame;
+import it.polimi.ingsw.model.immutable.ImmutablePlayer;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.SocketException;
+import java.util.List;
+
+public class SocketObserver implements Observer{
+    String nickname;
+    ObjectOutputStream oos;
+
+    public SocketObserver(String nickname, ObjectOutputStream oos) {
+        this.nickname = nickname;
+        this.oos = oos;
+    }
+
+
+    public String getNickname() {
+        return nickname;
+    }
+
+
+    @Override
+    public void send_lobby_status(ImmutableLobby lobby) {
+        try {
+            oos.reset();
+            oos.writeObject(new NotifyMessage(NotifyType.LOBBY_STATUS,lobby));
+        }catch (SocketException exception){
+            //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void send_game_status(ImmutableGame game) {
+        try {
+            oos.reset();
+            oos.writeObject(new NotifyMessage(NotifyType.GAME_STATUS,game));
+        }catch (SocketException exception){
+            //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void send_player_status(ImmutablePlayer player) {
+        try {
+            oos.reset();
+            oos.writeObject(new NotifyMessage(NotifyType.PLAYER_STATUS,player));
+        }catch (SocketException exception){
+            //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void send_two_personal_goal(Integer[] personalGoals) {
+        try {
+            oos.writeObject(new NotifyMessage(NotifyType.PERSONAL_GOALS,personalGoals));
+        }catch (SocketException exception){
+            //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void send_player_handCard(List<Integer> handCards) {
+        try {
+            oos.writeObject(new NotifyMessage(NotifyType.HAND_CARDS, handCards));
+        }catch (SocketException exception){
+            //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void send_end_game_info(ImmutableEndGameInfo endGameInfo) {
+        try {
+            oos.writeObject(new NotifyMessage(NotifyType.END_GAME_INFO,endGameInfo));
+        }catch (SocketException exception){
+            //
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ObjectOutputStream getOos() {
+        return oos;
+    }
+}
