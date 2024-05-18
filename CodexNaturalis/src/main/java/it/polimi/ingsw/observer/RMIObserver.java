25728985e7f0c0e03422b5e775e65a9b2913377d
Valeria Lu <valeria.lu@mail.polimi.it>
2024-05-18 03:32:36 +0200

    add empty RMI classes (to be completed by Nourhane)

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/observer/RMIObserver.java
@@ -0,0 +1,46 @@
+package it.polimi.ingsw.observer;
+
+import it.polimi.ingsw.controller.server.ImmutableLobby;
+import it.polimi.ingsw.model.immutable.ImmutableEndGameInfo;
+import it.polimi.ingsw.model.immutable.ImmutableGame;
+import it.polimi.ingsw.model.immutable.ImmutablePlayer;
+
+import java.util.List;
+
+//to be completed by Nourhane
+public class RMIObserver implements Observer{
+    @Override
+    public String getNickname() {
+        return null;
+    }
+
+    @Override
+    public void send_lobby_status(ImmutableLobby lobby) {
+
+    }
+
+    @Override
+    public void send_game_status(ImmutableGame game) {
+
+    }
+
+    @Override
+    public void send_player_status(ImmutablePlayer player) {
+
+    }
+
+    @Override
+    public void send_two_personal_goal(Integer[] personalGoals) {
+
+    }
+
+    @Override
+    public void send_player_handCard(List<Integer> handCards) {
+
+    }
+
+    @Override
+    public void send_end_game_info(ImmutableEndGameInfo endGameInfo) {
+
+    }
+}
