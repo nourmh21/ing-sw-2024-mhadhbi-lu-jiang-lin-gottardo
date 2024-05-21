c9d62e836b5dd2e5a55466830e48307eca897b82
Elena <elena3.lin@mail.polimi.it>
2024-05-21 22:38:54 +0200

    add gui files and controller classes

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/ChooseInitialCardController.java
@@ -0,0 +1,46 @@
+package it.polimi.ingsw.view.GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class ChooseInitialCardController {
+
+    @FXML
+    private ImageView initialBack1;
+
+    @FXML
+    private ImageView initialBack2;
+
+    @FXML
+    private ImageView initialFront1;
+
+    @FXML
+    private ImageView initialFront2;
+
+    @FXML
+    void getBack1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getBack2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getFront1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getFront2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToNextChoice(ActionEvent event) {
+
+    }
+
+}
