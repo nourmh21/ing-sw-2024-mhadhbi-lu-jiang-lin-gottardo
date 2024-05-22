59244fe4cddea759e1053d0aa3ff2eb1b8c269fc
Valeria Lu <valeria.lu@mail.polimi.it>
2024-05-22 19:08:42 +0200

    update gui classes and rename fxml files

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controllers/For2PlayersLobby.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw.view.GUI.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.List;
+
+public class For2PlayersLobby {
+
+    @FXML
+    private ImageView imgFirstPlayer;
+    @FXML
+    private ImageView imgSecondPlayer;
+
+    @FXML
+    private Label nickFirstPlayer;
+    @FXML
+    private Label nickSecondPlayer;
+
+    private List<String> players;
+
+    Image playerIcon = new Image(getClass().getResourceAsStream("/img/utils/playersIcon.png"));
+
+    public void updateStatus(List<String> newStatus){
+        players = newStatus;
+        if (players.size() == 1){
+            updatePlayer1();
+        }else  if (players.size() == 2){
+            updatePlayer1();
+            updatePlayer2();
+        }
+    }
+
+    private void updatePlayer1(){
+        imgFirstPlayer.setImage(playerIcon);
+        nickFirstPlayer.setText(players.get(0));
+    }
+
+    private void updatePlayer2(){
+        imgSecondPlayer.setImage(playerIcon);
+        nickSecondPlayer.setText(players.get(1));
+    }
+
+
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controllers/ForHome.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.view.GUI.controllers;
+
+import it.polimi.ingsw.controller.client.ClientController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import java.io.IOException;
+
+public class ForHome {
+
+    @FXML
+    private Button play;
+
+    @FXML
+    void playGame(ActionEvent event) throws IOException {
+        ClientController.getInstance().getClientAction().joinGame();
+    }
+
+}
