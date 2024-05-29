290eceed7e65a83368fd6457aafd89a907bba73c
Valeria Lu <valeria.lu@mail.polimi.it>
2024-05-30 01:13:33 +0200

    fix a class code in controller package

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/server/task/EndGame.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/server/task/EndGame.java
@@ -2,11 +2,12 @@ package it.polimi.ingsw.controller.server.task;
 
 import it.polimi.ingsw.controller.server.GameController;
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.ObjectiveCard;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.enums.GameState;
 
 public class EndGame implements Runnable{
-    private Game game;
+    private final Game game;
 
     public EndGame(Game game){
         this.game = game;
@@ -16,6 +17,12 @@ public class EndGame implements Runnable{
     public void run() {
 
         if (game.getGameState() == GameState.ENDING){
+
+            for (Player p: game.getPlayers()) {
+                p.addGoalPoint(p.getBoard().calculateGoalPoint((ObjectiveCard)GameController.getInstance().getCard(game.getCommonGoals().get(0))));
+                p.addGoalPoint(p.getBoard().calculateGoalPoint((ObjectiveCard)GameController.getInstance().getCard(game.getCommonGoals().get(1))));
+                p.addGoalPoint(p.getBoard().calculateGoalPoint((ObjectiveCard)GameController.getInstance().getCard(p.getPersonalGoal())));
+            }
 
             game.checkMaxPoint();
 
