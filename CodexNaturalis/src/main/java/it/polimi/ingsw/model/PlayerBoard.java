74b1ff7048871e31047f56fd219f38f88ffe4688
nourmh21 <nourhane.mhadhbi@mail.polimi.it>
2024-05-31 14:29:07 +0200

    update PlayerBoard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerBoard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerBoard.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.model.enums.Symbol;
 import it.polimi.ingsw.model.exceptions.IllegalCoordinateInsertionException;
 import it.polimi.ingsw.model.exceptions.InvalidIdCardException;
+import static it.polimi.ingsw.model.enums.Symbol.*;
 
 import java.util.*;
 
-import static it.polimi.ingsw.model.enums.Symbol.*;
 
 public class PlayerBoard {
 
@@ -16,14 +16,14 @@ public class PlayerBoard {
     final private List<Symbol> bottomLeftAngle;
     final private List<Symbol> bottomRightAngle;
     final private List<Symbol> cardKingdom;
+    final private List<Boolean> cardSide;
 
-    List<Boolean> cardSide;
+
     final private List<Integer>  x;
     final private List<Integer> y;
-    final private int[] symbolsList;
-    private List<int[]> availablePosition;
+    final private int[] symbolList;
+    private final List<int[]> availablePosition;
     final private ArrayList<int[]> forbiddenPosition;
-
     private boolean isInitCardPlaced;
 
     /**
@@ -35,14 +35,14 @@ public class PlayerBoard {
      * isCoveredBottomLeftAngle whether the corner is covered or show the resource
      * isCoveredBottomRightAngle whether the corner is covered or show the resource
      * cardKingdom defines the color of the card
-     * symbolsList define the resources and objects visible in the PlayerBoard.
-     * symbolsList[0] indicates the number of ANIMAL,
-     * symbolsList[1] indicates the number of PLANT,
-     * symbolsList[2] indicates the number of FUNGI,
-     * symbolsList[3] indicates the number of INSECT,
-     * symbolsList[4] indicates the number of FEATHER,
-     * symbolsList[5] indicates the number of INK_BOTTLE,
-     * symbolsList[6] indicates the number of PARCHMENT,
+     * symbolList define the resources and objects visible in the PlayerBoard.
+     * symbolList[0] indicates the number of ANIMAL,
+     * symbolList[1] indicates the number of PLANT,
+     * symbolList[2] indicates the number of FUNGI,
+     * symbolList[3] indicates the number of INSECT,
+     * symbolList[4] indicates the number of FEATHER,
+     * symbolList[5] indicates the number of INK_BOTTLE,
+     * symbolList[6] indicates the number of PARCHMENT,
      * availablePosition list of Position where player can place a new card
      * forbiddenPosition list of Position where player can't place a new card
      */
@@ -56,7 +56,7 @@ public class PlayerBoard {
         bottomRightAngle = new ArrayList<>();
         cardSide = new ArrayList<>();
         cardKingdom = new ArrayList<>();
-        symbolsList = new int[7];
+        symbolList = new int[7];
         availablePosition = new ArrayList<>();
         forbiddenPosition= new ArrayList<>();
         isInitCardPlaced = false;
@@ -93,9 +93,10 @@ public class PlayerBoard {
         y.add(yy);
         addSymbolsList(card,isBackSide,xx,yy);
         updateAvailablePosition();
-        if (isBackSide)
+        if (isBackSide){
+            calculatePoint(card , index);
             return 0;
-        else
+        } else
             return calculatePoint(card , index);
     }
 
@@ -120,10 +121,20 @@ public class PlayerBoard {
         addAvailablePosition(0,0);
 
         if (isBackSide) {
-            symbolsList[0] = initialCard.getCenterResource()[0];
-            symbolsList[1] = initialCard.getCenterResource()[1];
-            symbolsList[2] = initialCard.getCenterResource()[2];
-            symbolsList[3] = initialCard.getCenterResource()[3];
+            symbolList[0] = 1;
+            symbolList[1] = 1;
+            symbolList[2] = 1;
+            symbolList[3] = 1;
+            topLeftAngle.add(initialCard.getTopLeftAngle());
+            topRightAngle.add(initialCard.getTopRightAngle());
+            bottomLeftAngle.add(initialCard.getBottomLeftAngle());
+            bottomRightAngle.add(initialCard.getBottomRightAngle());
+
+        } else {
+            symbolList[0] = initialCard.getCenterResource()[0];
+            symbolList[1] = initialCard.getCenterResource()[1];
+            symbolList[2] = initialCard.getCenterResource()[2];
+            symbolList[3] = initialCard.getCenterResource()[3];
             topLeftAngle.add(initialCard.getTopLeftAngleFront());
             topRightAngle.add(initialCard.getTopRightAngleFront());
             bottomLeftAngle.add(initialCard.getBottomLeftAngleFront());
@@ -135,25 +146,14 @@ public class PlayerBoard {
                 forbiddenPosition.add(new int[]{1, -1});
             }
 
-
-        } else {
-            symbolsList[0] = 1;
-            symbolsList[1] = 1;
-            symbolsList[2] = 1;
-            symbolsList[3] = 1;
-            topLeftAngle.add(initialCard.getTopLeftAngle());
-            topRightAngle.add(initialCard.getTopRightAngle());
-            bottomLeftAngle.add(initialCard.getBottomLeftAngle());
-            bottomRightAngle.add(initialCard.getBottomRightAngle());
-
-            }
-            symbolsList[4] = 0;
-            symbolsList[5] = 0;
-            symbolsList[6] = 0;
+        }
+            symbolList[4] = 0;
+            symbolList[5] = 0;
+            symbolList[6] = 0;
             isInitCardPlaced = true;
             return 0;
-
     }
+
 
     public List<Integer> getY() {
         return y;
@@ -176,7 +176,6 @@ public class PlayerBoard {
             bottomRightAngle.add(EMPTY);
             topLeftAngle.add( EMPTY);
             topRightAngle.add(EMPTY);
-
 
         } else {
             editAddSymbolsList(card.getTopLeftAngle(),new int[]{xx-1,yy+1});
@@ -203,7 +202,7 @@ public class PlayerBoard {
         for (int elem : x) {
             //int index = x.indexOf(elem);
             if (elem == (xx - 1) & y.get(index) == (yy + 1)) {
-                editRemoveSymbolsList(bottomRightAngle.get(index));
+                editRemoveSymbolList(bottomRightAngle.get(index));
                 bottomRightAngle.set(index, COVERED_ANGLE);
                 //also the card that I place even if the resource is not covered, I need it to calculate point
                 topLeftAngle.set(i, COVERED_ANGLE);
@@ -211,21 +210,21 @@ public class PlayerBoard {
 
             }
             if (elem == (xx + 1) & y.get(index) == (yy + 1)) {
-                editRemoveSymbolsList(bottomLeftAngle.get(index));
+                editRemoveSymbolList(bottomLeftAngle.get(index));
                 bottomLeftAngle.set(index, COVERED_ANGLE);
                 topRightAngle.set(i, COVERED_ANGLE);
                 removeAvailablePosition(new int[]{xx+1,yy+1});
 
             }
             if (elem == (xx + 1) & y.get(index) == (yy - 1)) {
-                editRemoveSymbolsList(topLeftAngle.get(index));
+                editRemoveSymbolList(topLeftAngle.get(index));
                 topLeftAngle.set(index, COVERED_ANGLE);
                 bottomRightAngle.set(i, COVERED_ANGLE);
                 removeAvailablePosition(new int[]{xx+1,yy-1});
 
             }
             if (elem == (xx - 1) & y.get(index) == (yy - 1)) {
-                editRemoveSymbolsList(topRightAngle.get(index));
+                editRemoveSymbolList(topRightAngle.get(index));
                 topRightAngle.set(index,COVERED_ANGLE);
                 bottomLeftAngle.set(i, COVERED_ANGLE);
                 removeAvailablePosition(new int[]{xx-1,yy-1});
@@ -242,7 +241,6 @@ public class PlayerBoard {
      * @return list of AvailablePosition
      */
     public List<int[]> getAvailablePosition() {
-
         return availablePosition;
     }
 
@@ -250,15 +248,15 @@ public class PlayerBoard {
     /**
      * @return SymbolsList
      */
-    public int[] getSymbolsList(){
-        return symbolsList;
+    public int[] getSymbolList(){
+        return symbolList;
     }
 
 
     /**
      * @return list of board cards
      */
-    public List<Integer> getBoardCards() {
+    public List<Integer> getBoardCards(){
         return boardCards;
     }
 
@@ -290,15 +288,15 @@ public class PlayerBoard {
                         if (bottomLeftAngle.get(i)==COVERED_ANGLE) x += 2;
                         break;
                     case FEATHER:
-                        x = symbolsList[4] * card.getPoint();
+                        x = symbolList[4] * card.getPoint();
                         removeSymbolsList( i);
                         break;
                     case PARCHMENT:
-                        x = symbolsList[6] * card.getPoint();
+                        x = symbolList[6] * card.getPoint();
                         removeSymbolsList( i);
                         break;
                     case INK_BOTTLE:
-                        x = symbolsList[5] * card.getPoint();
+                        x = symbolList[5] * card.getPoint();
                         removeSymbolsList(i);
                         break;
                 }
@@ -343,31 +341,31 @@ public class PlayerBoard {
                     point = goalCardPosition(FUNGI,ANIMAL, ANIMAL, -1, -1, 0,-2);
                     break;
                 case FFF:
-                    point = 2 * (symbolsList[2] / 3);
+                    point = 2 * (symbolList[2] / 3);
                     break;
                 case AAA:
-                    point = 2 * (symbolsList[0] / 3);
+                    point = 2 * (symbolList[0] / 3);
                     break;
                 case PPP:
-                    point = 2 * (symbolsList[1] / 3);
+                    point = 2 * (symbolList[1] / 3);
                     break;
                 case III:
-                    point = 2 * (symbolsList[3] / 3);
+                    point = 2 * (symbolList[3] / 3);
                     break;
                 case BFP:
-                    int min = symbolsList[4];
-                    if (min > symbolsList[5]) min = symbolsList[5];
-                    if (min > symbolsList[6]) min = symbolsList[6];
+                    int min = symbolList[4];
+                    if (min > symbolList[5]) min = symbolList[5];
+                    if (min > symbolList[6]) min = symbolList[6];
                     point = min * 3;
                     break;
                 case BB:
-                    point = 2 * (symbolsList[5] / 2);
+                    point = 2 * (symbolList[5] / 2);
                     break;
                 case FF:
-                    point = 2 * (symbolsList[4] / 2);
+                    point = 2 * (symbolList[4] / 2);
                     break;
                 case PP:
-                    point = 2 * (symbolsList[6] / 2);
+                    point = 2 * (symbolList[6] / 2);
                     break;
             }
 
@@ -392,25 +390,25 @@ public class PlayerBoard {
                 forbiddenPosition.add(position);
                 break;
             case ANIMAL:
-                symbolsList[0] += 1;
+                symbolList[0] += 1;
                 break;
             case PLANT:
-                symbolsList[1] += 1;
+                symbolList[1] += 1;
                 break;
             case FUNGI:
-                symbolsList[2] += 1;
+                symbolList[2] += 1;
                 break;
             case INSECT:
-                symbolsList[3] += 1;
+                symbolList[3] += 1;
                 break;
             case FEATHER:
-                symbolsList[4] += 1;
+                symbolList[4] += 1;
                 break;
             case INK_BOTTLE:
-                symbolsList[5] += 1;
+                symbolList[5] += 1;
                 break;
             case PARCHMENT:
-                symbolsList[6] += 1;
+                symbolList[6] += 1;
                 break;
 
 
@@ -423,30 +421,30 @@ public class PlayerBoard {
      * remove resources to the symbol list
      * @param symbol to remove from the SymbolList
      */
-    private void editRemoveSymbolsList(Symbol symbol) {
+    private void editRemoveSymbolList(Symbol symbol) {
         switch (symbol) {
             case EMPTY:
                 break;
             case ANIMAL:
-                symbolsList[0] -= 1;
+                symbolList[0] -= 1;
                 break;
             case PLANT:
-                symbolsList[1] -= 1;
+                symbolList[1] -= 1;
                 break;
             case FUNGI:
-                symbolsList[2] -= 1;
+                symbolList[2] -= 1;
                 break;
             case INSECT:
-                symbolsList[3] -= 1;
+                symbolList[3] -= 1;
                 break;
             case FEATHER:
-                symbolsList[4] -= 1;
+                symbolList[4] -= 1;
                 break;
             case INK_BOTTLE:
-                symbolsList[5] -= 1;
+                symbolList[5] -= 1;
                 break;
             case PARCHMENT:
-                symbolsList[6] -= 1;
+                symbolList[6] -= 1;
                 break;
 
         }
@@ -536,26 +534,16 @@ public class PlayerBoard {
      * remove forbidden position from available position list
      */
     private void updateAvailablePosition(){
-        update(forbiddenPosition);
-        List<int[]> placedPositions = new ArrayList<>();
-        for (int i = 0; i < x.size(); i++) {
-            placedPositions.add(new int[]{x.get(i), y.get(i)});
-        }
-        update(placedPositions);
-    }
-
-    private void update(List<int[]> list){
-        for (int[] elementToControlled : list) {
-            for (int j = 0; j < availablePosition.size(); j++) {
-                int[] arrayAvailablePosition = availablePosition.get(j);
-                if (Arrays.equals(elementToControlled, arrayAvailablePosition)) {
-                    availablePosition.remove(j);
-                    break;
+        for (int[] elementToControlled : forbiddenPosition) {
+                for (int j = 0; j < availablePosition.size(); j++) {
+                    int[] arrayAvailablePosition = availablePosition.get(j);
+                    if (Arrays.equals(elementToControlled, arrayAvailablePosition)) {
+                        availablePosition.remove(j);
+                        j--;
+                    }
                 }
             }
-        }
     }
-
 
 
     /**
@@ -567,16 +555,16 @@ public class PlayerBoard {
             int[] array = availablePosition.get(i);
             if (Arrays.equals(array, position)) {
                 availablePosition.remove(i);
-                break;
+                i--;
             }
         }
     }
 
     public boolean checkGoldCardCondition(GoldCard card){
-        return card.getCondition()[0] <= symbolsList[0]
-                & card.getCondition()[1] <= symbolsList[1]
-                & card.getCondition()[2] <= symbolsList[2]
-                & card.getCondition()[3] <= symbolsList[3];
+        return card.getCondition()[0] <= symbolList[0]
+                & card.getCondition()[1] <= symbolList[1]
+                & card.getCondition()[2] <= symbolList[2]
+                & card.getCondition()[3] <= symbolList[3];
     }
 
 }
