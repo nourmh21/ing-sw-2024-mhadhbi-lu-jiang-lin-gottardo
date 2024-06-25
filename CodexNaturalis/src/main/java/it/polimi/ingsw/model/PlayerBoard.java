e70cffd4476e4ec55773828f9f1c1a0025e0a0af
nourmh21 <nourhane.mhadhbi@mail.polimi.it>
2024-06-25 11:53:44 +0200

    update PlayerBoard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerBoard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayerBoard.java
@@ -94,8 +94,7 @@ public class PlayerBoard {
         addSymbolsList(card,isBackSide,xx,yy);
         updateAvailablePosition();
         if (isBackSide){
-            //removeSymbolsList(index);
-            calculatePoint(card , index);
+            removeSymbolsList(index);
             return 0;
         } else
             return calculatePoint(card , index);
