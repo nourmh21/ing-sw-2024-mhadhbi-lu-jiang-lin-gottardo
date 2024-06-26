e79b3732c0eb64a6c4de77de6c8bdfc92c993603
nourmh21 <nourhane.mhadhbi@mail.polimi.it>
2024-06-26 12:28:45 +0200

    update ClientApp

--- CodexNaturalis/src/main/java/it/polimi/ingsw/main/ClientApp.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/main/ClientApp.java
@@ -15,21 +15,28 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 
 public class ClientApp {
 
+    /**
+     * The main entry point of the client application.
+     * @param args the command-line arguments
+     */
     public static void main( String[] args ) {
         boolean isTUI = false;
+
         for (String arg : args) {
             if (arg.equals("--tui")) {
                 isTUI = true;
                 break;
             }
         }
-        //da togliere
+
         if (isTUI){
             TUI tui = new TUI();
             ClientController.getInstance().setView(tui);
@@ -42,6 +49,10 @@ public class ClientApp {
     }
 
 
+    /**
+     * Sets up the socket client by connecting to the socket server located at the specified IP and port.
+     * @param ip the IP address of the socket server.
+     */
     public static void trySocketConnection(String ip){
         try {
             Socket socket = new Socket(ip, 49257);
@@ -56,11 +67,16 @@ public class ClientApp {
     }
 
 
-    public static void tryRMIConnection(String ip){
-
-        RMIClientImpl client= null;
+    /**
+     * Sets up the RMI client by connecting to the RMI registry located at the specified IP and port.
+     * Throws exceptions if the IP address or port is invalid or if the RMI registry is not bound.
+     * @param ip the IP address of the RMI registry.
+     * @throws RemoteException if a remote communication error occurs.
+     * @throws NotBoundException if an attempt is made to lookup in the registry a name that has no associated binding.
+     */
+    public static void tryRMIConnection(String ip) throws RemoteException , NotBoundException {
         try {
-                client = new RMIClientImpl();
+                RMIClientImpl client= new RMIClientImpl();
                 // Getting the registry.
                 Registry registry = LocateRegistry.getRegistry(ip, 1099);
                 // Looking up the registry for the remote object
@@ -68,12 +84,15 @@ public class ClientApp {
                 ClientController.getInstance().setClientAction(new RMIAction( stub,client));
                 stub.registerClient(client);
         } catch (Exception e) {
-                System.err.println("ClientApp exception: " + e.toString());
+                System.err.println("ClientApp exception: " + e);
                 ClientController.getInstance().getView().showConnectionError();
         }
     }
 
 
+    /**
+     * check if the provided IP address is valid.
+     */
     public static boolean checkIPValidity(String ip){
         if (ip.isEmpty()) {
             return false;
@@ -92,7 +111,6 @@ public class ClientApp {
         }
         return true;
     }
-
 
 
 }
