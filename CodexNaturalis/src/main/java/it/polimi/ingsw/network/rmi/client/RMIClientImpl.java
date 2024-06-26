5e2ad57bf4484509f9305d7bcf4daf20d2711b77
nourmh21 <nourhane.mhadhbi@mail.polimi.it>
2024-06-26 12:37:52 +0200

    update RMIClientImpl

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/client/RMIClientImpl.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/client/RMIClientImpl.java
@@ -6,19 +6,31 @@ import it.polimi.ingsw.message.Message;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+/**
+ * The RMIClientImpl class is an implementation of the {@link RMIClientInterface } interface.
+ * It extends the {@link UnicastRemoteObject} class .
+ */
+
 public class RMIClientImpl extends UnicastRemoteObject implements RMIClientInterface {
 
-    private MessageExecutor messageExecutor;
+    private final MessageExecutor messageExecutor;
+
 
     public RMIClientImpl() throws RemoteException {
-
         messageExecutor = new MessageExecutor();
     }
 
-
+    /**
+     * server can invoke periodically to check if the client is still connected.
+     * @throws RemoteException if a remote communication error occurs
+     */
     @Override
     public void ping() throws RemoteException {}
 
+    /**
+     * update the client with a {@link Message}
+     * @throws RemoteException if a remote communication error occurs
+     */
     @Override
     public void executeMessage(Message message) throws RemoteException {
         messageExecutor.execute(message);
--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/client/RMIClientInterface.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/rmi/client/RMIClientInterface.java
@@ -5,7 +5,13 @@ import it.polimi.ingsw.message.Message;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * The RMIClientInterface  represents a client in a network communication.
+ * It extends the {@link Remote} interface and provides a method for updating the client with a {@link Message}.
+ */
+
 public interface RMIClientInterface extends Remote {
     void ping() throws RemoteException;
     void executeMessage(Message message)throws RemoteException;
+
 }
